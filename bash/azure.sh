27f5946f455d8a7b498e937fef753a6d864162c8
WYANG008 <weitao.yang@finbook.co>
2018-06-08 13:39:14 +0800

    update bash

--- bash/azure.sh
+++ bash/azure.sh
@@ -4,4 +4,4 @@ npm run gemini azure &>> gemini.log &
 npm run kraken azure &>> kraken.log &
 npm run subscribe azure event=StartPreReset source=infura &>> preReset.log &
 npm run subscribe azure event=StartReset source=infura &>> reset.log &
-npm run commit azure source=infura generator=preSet &>> commit.log &
+npm run commit azure source=infura generator=gbm &>> commit.log &
