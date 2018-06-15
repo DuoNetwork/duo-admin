5e50e52489c066a14b30e0b6c4d0ae5e860ec341
WYANG008 <weitao.yang@finbook.co>
2018-06-15 14:50:19 +0800

    change gasLimit and remove public chain process

--- bash/public.sh
+++ bash/public.sh
@@ -2,7 +2,6 @@ npm run bitfinex dynamo &>> bitfinex.log &
 npm run gdax dynamo &>> gdax.log &
 npm run gemini dynamo &>> gemini.log &
 npm run kraken dynamo &>> kraken.log &
-npm run node dynamo &>> node.log &
 npm run hourly dynamo &>> node.log &
 npm run minutely dynamo &>> node.log &
 npm run subscribe event=others source=infura dynamo &>> others.log &
