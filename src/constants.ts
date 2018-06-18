89de5ff605b5b52faedcf9847c6804b6dc7dc80d
yizhoucao <yizhou.cao@finbook.co>
2018-06-18 17:06:09 +0800

    add ui event db constant

--- src/constants.ts
+++ src/constants.ts
@@ -62,6 +62,8 @@ export const DB_EV_TOKEN_B = 'tokenBInWei';
 export const DB_EV_ETH = 'ethAmtInWei';
 export const DB_EV_TOTAL_SUPPLY_A = 'totalSupplyA';
 export const DB_EV_TOTAL_SUPPLY_B = 'totalSupplyB';
+export const DB_EV_UI_ETH = 'eth';
+export const DB_EV_UI_AB = 'ab';
 export const DB_STATUS_EVENT_PUBLIC_OTHERS = 'EVENT_AWS_PUBLIC_OTHERS';
 
 export const AWS_DYNAMO_ROLE_TRADE = 'trade';
