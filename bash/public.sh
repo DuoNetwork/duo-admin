50f59b564c947f76afe1fc0babee2a18d9de7555
WYANG008 <weitao.yang@finbook.co>
2018-06-19 15:50:29 +0800

    change bash

--- bash/public.sh
+++ bash/public.sh
@@ -2,6 +2,6 @@ npm run bitfinex server dynamo &>> bitfinex.log &
 npm run gdax server dynamo &>> gdax.log &
 npm run gemini server dynamo &>> gemini.log &
 npm run kraken server dynamo &>> kraken.log &
-npm run hourly server dynamo &>> node.log &
-npm run minutely server dynamo &>> node.log &
+npm run hourly server dynamo &>> hourly.log &
+npm run minutely server dynamo &>> minutely.log &
 npm run subscribe server event=others source=infura dynamo &>> others.log &
