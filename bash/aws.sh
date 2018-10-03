94e579f1efb08461379a4a6d70c0eb241614087b
WYANG008 <weitao.yang@finbook.co>
2018-10-03 16:52:59 +0800

    add node listerner in the bash

--- bash/aws.sh
+++ bash/aws.sh
@@ -3,5 +3,6 @@ npm run trades assets=ETH,USD server aws &> trades.ALL.log &
 npm run subscribe server aws event=StartPreReset &>> preReset.log &
 npm run subscribe server aws event=StartReset &>> reset.log &
 npm run commit server aws &>> commit.log &
+npm run node server aws &>> node.log &
 npm run cleanDB aws &>> cleanDB.log &
 
