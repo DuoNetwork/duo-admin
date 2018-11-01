e475962c6606e3b989c1eb479fe5d449845d049c
WYANG008 <weitao.yang@finbook.co>
2018-11-01 10:52:14 +0800

    remove node process from the bash

--- bash/aws.sh
+++ bash/aws.sh
@@ -4,6 +4,5 @@ npm run trades assets=ETH,USD server aws &> trades.ALL.log &
 npm run subscribe server aws event=StartPreReset &>> preReset.log &
 npm run subscribe server aws event=StartReset &>> reset.log &
 npm run commit server aws base=USD quote=ETH &>> commit.log &
-npm run node server aws &>> node.log &
 npm run cleanDB server aws &>> cleanDB.log &
 
