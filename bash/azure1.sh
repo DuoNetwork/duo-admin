8dde036a1c66f93b8de68bc3b6aaaac23b6e7a46
WYANG008 <weitao.yang@finbook.co>
2019-02-01 16:26:11 +0800

    minor change

--- bash/azure1.sh
+++ bash/azure1.sh
@@ -1,4 +1,5 @@
 killall -s KILL node
 rm *.log
 npm run trades assets=ETH,USD server azure &> trades.ALL.log &
+npm run commit server azure pair=ETH_USD &>> commit.log &
 npm run cleanDB server azure &>> cleanDB.log &
--- bash/azure2.sh
+++ bash/azure2.sh
@@ -1,4 +1,3 @@
 npm run trigger server azure event=StartPreReset &>> preReset.log &
 npm run trigger server azure event=StartReset &>> reset.log &
-npm run commit server azure pair=ETH_USD &>> commit.log &
 npm run fetchPrice server azure &>> fetchPrice.log &
