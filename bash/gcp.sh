476ff0953c91f00dc225828eb2b3f0b6d801030c
WYANG008 <weitao.yang@finbook.co>
2019-02-26 15:42:57 +0800

    update bash

--- bash/gcp.sh
+++ bash/gcp.sh
@@ -4,4 +4,5 @@ npm run trades assets=ETH,USD server gcp &> trades.ALL.log &
 npm run commit server gcp pair=ETH_USD &>> commit.log &
 npm run cleanDB server gcp &>> cleanDB.log &
 npm run round contractType=Vivaldi tenor=100C-3H debug server gcp &>> vivaldi-100C-3H.log &
+npm run fetchPrice server gcp &>> fetchPrice.log &
 npm run events server gcp events=StartReset,StartPreReset $1 &>> events.log &
