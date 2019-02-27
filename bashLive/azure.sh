aa6f6630879309d1ce8696851c428a218148b8a9
WYANG008 <weitao.yang@finbook.co>
2019-02-27 11:12:43 +0800

    update liveBash

--- bashLive/azure.sh
+++ bashLive/azure.sh
@@ -1,8 +1,4 @@
 killall -s KILL node
 rm *.log
 npm run trades assets=ETH,USD server azure live &> trades.ALL.log &
-npm run commit server azure live pair=ETH_USD &>> commit.log &
 npm run cleanDB server azure live &>> cleanDB.log &
-npm run round contractType=Vivaldi tenor=100C-3H debug server azure live &>> vivaldi-100C-3H.log &
-npm run fetchPrice server azure live &>> fetchPrice.log &
-npm run events server azure live events=StartReset,StartPreReset $1 &>> events.log &
--- bashLive/public.sh
+++ bashLive/public.sh
@@ -3,4 +3,3 @@ rm *.log
 npm run trades assets=ETH,USD server dynamo live &> trades.ALL.log &
 npm run prices period=1 dynamo live &> prices.1.log &
 npm run prices period=60 dynamo live &> prices.60.log &
-npm run events server dynamo events=Others live $1 &>> events.log &
