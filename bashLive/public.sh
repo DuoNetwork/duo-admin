ff3764479087bec6dae2340c9cb7ea29639b4788
WYANG008 <weitao.yang@finbook.co>
2019-02-27 10:32:11 +0800

    minor change

--- bashLive/public.sh
+++ bashLive/public.sh
@@ -1,6 +1,6 @@
 killall -s KILL node
 rm *.log
 npm run trades assets=ETH,USD server dynamo live &> trades.ALL.log &
-npm run prices period=1 dev dynamo live &> prices.1.log &
-npm run prices period=60 dev dynamo live &> prices.60.log &
+npm run prices period=1 dynamo live &> prices.1.log &
+npm run prices period=60 dynamo live &> prices.60.log &
 npm run events server dynamo events=Others live $1 &>> events.log &
