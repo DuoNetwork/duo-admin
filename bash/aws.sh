f08db6565f7627b8a2cee1f347344ae2247be4e3
WYANG008 <weitao.yang@finbook.co>
2019-03-12 14:00:55 +0800

    update

--- bash/aws.sh
+++ bash/aws.sh
@@ -3,6 +3,6 @@ rm *.log
 npm run trades assets=ETH,USD server aws &> trades.ALL.log &
 npm run commit server aws pair=ETH_USD &>> commit.log &
 npm run cleanDB server aws &>> cleanDB.log &
-npm run round contractType=Vivaldi tenor=100C-3H debug server aws &>> vivaldi-100C-3H.log &
+npm run round contractType=Vivaldi tenor=100C-1H debug server aws &>> vivaldi-100C-1H.log &
 npm run fetchPrice server pair=ETH_USD aws &>> fetchPrice.log &
 npm run events server aws events=StartReset,StartPreReset $1 &>> events.log &
