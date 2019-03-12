02aa0448300cef5f7068017001a0aab9dee711d8
WYANG008 <weitao.yang@finbook.co>
2019-03-12 14:08:35 +0800

    update bash

--- bash/gcp.sh
+++ bash/gcp.sh
@@ -1,8 +1,8 @@
 killall -s KILL node
 rm *.log
 npm run trades assets=ETH,USD server gcp &> trades.ALL.log &
-npm run commit server gcp pair=ETH_USD &>> commit.log &
+npm run commit server gcp pairs=ETH_USD &>> commit.log &
 npm run cleanDB server gcp &>> cleanDB.log &
 npm run round contractType=Vivaldi tenor=100C-1H debug server gcp &>> vivaldi-100C-1H.log &
-npm run fetchPrice server pair=ETH_USD gcp &>> fetchPrice.log &
+npm run fetchPrice server pairs=ETH_USD gcp &>> fetchPrice.log &
 npm run events server gcp events=StartReset,StartPreReset $1 &>> events.log &
