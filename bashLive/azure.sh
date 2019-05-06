e41878bd961625312b665953b4ae801cad9de704
WYANG008 <weitao.yang@finbook.co>
2019-05-06 10:39:01 +0800

    update bash

--- bashLive/azure.sh
+++ bashLive/azure.sh
@@ -5,4 +5,3 @@ npm run cleanDB server azure live &>> cleanDB.log &
 npm run commit server azure live pairs=ETH_USD &>> commit.log &
 npm run fetchPrice server pairs=ETH_USD azure live &>> fetchPrice.log &
 npm run events server azure live events=StartReset,StartPreReset $1 &>> events.log &
-npm run round contractType=Vivaldi tenor=100C-1H server azure live &>> vivaldi-100C-1H.log &
